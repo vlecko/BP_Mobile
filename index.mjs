4a4b55e639330a1fab86359f7411e0bef0bfc3f3
vlad <letsko.vlad@gmail.com>
2023-09-29 16:27:47 +0300

    index.mjs

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import de from './src/i18n/de.json';
+import en from './src/i18n/en.json';
+import es from './src/i18n/es.json';
+import fr from './src/i18n/fr.json';
+import ja from './src/i18n/ja.json';
+import pt from './src/i18n/pt.json';
+
+export{ de, en, es, fr, ja, pt };
--- src/i18n/index.mjs
+++
@@ -1,8 +0,0 @@
-import de from './de.json';
-import en from './en.json';
-import es from './es.json';
-import fr from './fr.json';
-import ja from './ja.json';
-import pt from './pt.json';
-
-export{ de, en, es, fr, ja, pt };
