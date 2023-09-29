5556281a61476b16b59fb506d420e60158397e5e
vlad <letsko.vlad@gmail.com>
2023-09-29 16:32:03 +0300

    index.mjs

---
+++ src/i18n/index.mjs
@@ -0,0 +1,8 @@
+import de from './de.json';
+import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import ja from './ja.json';
+import pt from './pt.json';
+
+export{ de, en, es, fr, ja, pt };
